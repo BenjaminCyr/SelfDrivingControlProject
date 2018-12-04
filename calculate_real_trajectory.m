dc609e94627c2892d3a975c7bf546f7ded4a8b9b
BenjaminCyr <bencyr@umich.edu>
2018-12-04 11:35:31 -0500

    Added file to read Input, simulate, and get the trajectory info

---
+++ calculate_real_trajectory.m
@@ -0,0 +1,14 @@
+load('Input.mat');
+load('TestTrack.mat');
+
+Y = forwardIntegrateControlInput(U);
+
+plot(TestTrack.bl(1,:), TestTrack.bl(2,:), 'b')
+hold on
+plot(TestTrack.br(1,:), TestTrack.br(2,:), 'b')
+hold on
+plot(TestTrack.cline(1,:), TestTrack.cline(2,:), 'y')
+hold on
+plot(Y(:,1),Y(:,3), 'r');
+
+info = getTrajectoryInfo(Y, U, [], TestTrack)
