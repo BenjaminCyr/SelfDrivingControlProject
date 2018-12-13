0d427b5871e1fab26ac544ae56ed5a68a40339c8
BenjaminCyr <bencyr39@gmail.com>
2018-12-13 13:43:07 -0500

    Placeholder function for Part 2

---
+++ ROB599_ControlsProject_part2_Team35.m
@@ -0,0 +1,5 @@
+function U = ROB599_ControlsProject_part2_Team35(TestTrack, Xobs)
+    load('ROB599_ControlsProjectTeam35.mat', 'traj_total', 'ROB599_ControlsProject_part1_input');
+    
+    U = ROB599_ControlsProject_part1_input;
+end
